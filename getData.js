8b66112132b7a3859513c640e2583575a89a8935
nadwaso2000 <nadwaso2000@gmail.com>
2025-07-22 09:18:56 +0300

    update

--- getData.js
+++ getData.js
@@ -1,45 +1,60 @@
 let parentCards = document.getElementById('parentcards')
+let menu = document.querySelector('.menu')
+let ul = document.querySelector('ul')
+let picon = document.querySelector('.p-icon')
+
 let arrOfSavedProduct = []
 
-console.log(arrOfSavedProduct)
+menu.onclick = () =>
+{
+    ul.classList.toggle('active')
+    picon.classList.toggle('active2')
+
+}
 
 if(localStorage.getItem('products'))
 {
     arrOfSavedProduct = JSON.parse(localStorage.getItem("products"))
 }
 
-let ArrayOfObgects = [];
 
+let ArrayOfObjects = [];
 
  axios.get("https://fakestoreapi.com/products")
     .then((response) => {
         console.log(response.data)
-        ArrayOfObgects = response.data;
-        console.log(ArrayOfObgects)
-         for(let i=0 ; i< response.data.length ; i++)
+        ArrayOfObjects = response.data;
+        console.log(ArrayOfObjects)
+         for(let i=0 ; i< ArrayOfObjects.length ; i++)
           {
               parentCards.innerHTML +=
                  `<div class="cards">
-                      <img src="${response.data[i].image}" alt=''/>
-                      <h2>${response.data[i].title.slice(0 , 12)}</h2>
+                      <img src="${ArrayOfObjects[i].image}" alt=''/>
+                      <h2>${ArrayOfObjects[i].title.slice(0 , 12)}</h2>
                       <div class='ineer'>
-                          <p>${response.data[i].price}$</p>
-                          <i class="fa-solid fa-store" data-value=${response.data[i]} id=${response.data[i].id}></i>
+                          <p>${ArrayOfObjects[i].price}$</p>
+                          <i class="fa-solid fa-store" id=${ArrayOfObjects[i].id}></i>
                      </div>
                  </div>` 
+          }
 
-                document.getElementById(`${ArrayOfObgects[i].id}`).addEventListener('click' , function(e) {
-                   arrOfSavedProduct.push(ArrayOfObgects[i]);
+           ArrayOfObjects.forEach(element => {
+               document.getElementById(element.id).addEventListener('click' , function() {
+                   arrOfSavedProduct.push(element);
                    localStorage.setItem('products' , JSON.stringify(arrOfSavedProduct))
                    console.log(arrOfSavedProduct)
-                   alert('product Added sucessfully')
+                   alert('product have been added sucessfully')
                 })
-          }
+             }); 
      })
      .catch(err => {
         console.log(err)
     })
 
+    
+         
+
+           
 
 
 
